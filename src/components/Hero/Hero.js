a801811f45a1ff330a6899c52c2917dd0e8e8996
Moeen Mahmud <write2fahim007@gmail.com>
2021-10-29 13:07:08 +0600

    added hero banner

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,9 +1,33 @@
 import React from "react";
+import HeroImage from "../../assets/hero-bg.png";
 
 const Hero = () => {
   return (
-    <div>
-      <h1>This is hero component</h1>
+    <div
+      style={{
+        backgroundImage: `url("${HeroImage}")`,
+        height: "70vh",
+        backgroundPosition: "bottom",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+      className="flex justify-center"
+    >
+      <div className="pt-20 text-center">
+        <h3 className="text-gray-300 italic text-lg mb-3">
+          Walk your own path and be yourself
+        </h3>
+        <h1 className="text-white font-bold text-7xl mb-4">
+          Travel <span className="text-indigo-600">Solo</span>
+        </h1>
+        <p className="text-gray-200 font-semibold text-xl">
+          Going in a group is a great way for solo travelers to see the world.
+          <br />
+          Enjoy the ease of having all the details handled, and the peace
+          <br />
+          of mind knowing we're there for you wherever you go.
+        </p>
+      </div>
     </div>
   );
 };
