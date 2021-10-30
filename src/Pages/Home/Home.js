f9e54fadb2d67f470dd8f6d3374055eee9069eda
Moeen Mahmud <write2fahim007@gmail.com>
2021-10-30 20:38:06 +0600

    refactored home codes

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,13 @@
+// Importing React
 import React from "react";
+
+// Importing home components
 import AboutUs from "../../components/AboutUs/AboutUs";
 import Discount from "../../components/Discount/Discount";
 import Hero from "../../components/Hero/Hero";
 import PackageContainer from "../../components/PackageContainer/PackageContainer";
 
+// Main Home Component Container
 const Home = () => {
   return (
     <div>
