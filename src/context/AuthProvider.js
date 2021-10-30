e9bf16f4cf7b971fb27e974664a084a737cef309
Moeen Mahmud <write2fahim007@gmail.com>
2021-10-30 20:21:36 +0600

    refactored auth provider codes

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -1,8 +1,13 @@
+// Importing necessary modules
 import React, { createContext } from "react";
+
+// Importing firebase functionality from the hook
 import useFirebase from "../hooks/useFirebase";
 
+// Creating the context
 export const AuthContext = createContext();
 
+// Main Auth Provider component
 const AuthProvider = ({ children }) => {
   const allAuthContext = useFirebase();
   return (
