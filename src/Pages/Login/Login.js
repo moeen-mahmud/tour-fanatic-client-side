85f84c3d14af42c4706a6a0145953e1aaa57e63a
Moeen Mahmud <write2fahim007@gmail.com>
2021-10-30 20:41:51 +0600

    refactored Login codes

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,15 +1,26 @@
+// Importing React
 import React from "react";
+
+// Importing useAuth hook for authentication
 import useAuth from "../../hooks/useAuth";
+
+// Importing the Google icon
 import { FcGoogle } from "react-icons/fc";
+
+// Importing necessary hooks from react router
 import { useHistory, useLocation } from "react-router";
 
+// Main Login component
 const Login = () => {
+  // Getting the methods from the hook
   const { signInUsingGoogle } = useAuth();
   const location = useLocation();
   const history = useHistory();
 
+  // Setting up the redirect url
   const redirectUrl = location.state?.from || "/";
 
+  // Function for sign in with google
   const handleGoogleSignIn = () => {
     signInUsingGoogle().then((result) => {
       history.push(redirectUrl);
