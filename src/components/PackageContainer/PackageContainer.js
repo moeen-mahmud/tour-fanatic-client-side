36d0d2dd46c8805122ac95796655e1d5b160e881
Moeen Mahmud <write2fahim007@gmail.com>
2021-10-30 20:12:49 +0600

    refactored packagecontainer codes

--- src/components/PackageContainer/PackageContainer.js
+++ src/components/PackageContainer/PackageContainer.js
@@ -1,13 +1,27 @@
-import axios from "axios";
+// Importing necessary modules from react
 import React, { useEffect, useState } from "react";
+
+// Importing Axios
+import axios from "axios";
+
+// Importing the module from react router
 import { useHistory } from "react-router";
+
+// Importing Package component
 import Package from "../Package/Package";
+
+// Importing Spinner component
 import Spinner from "../Spinner/Spinner";
 
+// Main Package Container component
 const PackageContainer = () => {
+  // Set all package in the state
   const [packages, setPackages] = useState([]);
+
+  // Using history for rerouting
   const history = useHistory();
 
+  // Using useEffect hook for fetching data from API server
   useEffect(() => {
     axios
       .get("https://young-wildwood-02340.herokuapp.com/packages")
@@ -16,7 +30,9 @@ const PackageContainer = () => {
       });
   }, []);
 
+  // Give booking button
   const handleBooking = (id) => {
+    // Get package by its id
     axios
       .get(`https://young-wildwood-02340.herokuapp.com/packages/${id}`)
       .then((res) => {
@@ -27,6 +43,7 @@ const PackageContainer = () => {
 
   return (
     <div className="p-10 my-8">
+      {/* Container title */}
       <h1 className="text-center font-semibold text-3xl lg:text-5xl text-gray-800">
         We've got some great deals
       </h1>
@@ -34,9 +51,11 @@ const PackageContainer = () => {
         Discover Now
       </p>
       {packages.length === 0 ? (
+        // Showing a loading spinner while data is loading
         <Spinner></Spinner>
       ) : (
         <div className="grid grid-cols-1 gap-10  lg:grid-cols-3 ">
+          {/* Mapping through all packages and set it in Package component */}
           {packages.map((pack) => (
             <Package
               pack={pack}
