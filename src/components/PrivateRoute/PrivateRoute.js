44097ed84c12d5d699aa5621e258f31abba72c0b
Moeen Mahmud <write2fahim007@gmail.com>
2021-10-30 20:18:11 +0600

    refactored private route codes

--- src/components/PrivateRoute/PrivateRoute.js
+++ src/components/PrivateRoute/PrivateRoute.js
@@ -1,14 +1,26 @@
+// Importing React
 import React from "react";
+
+// Importing modules from react rounter
 import { Redirect, Route } from "react-router";
+
+// Importing authentication from the hook
 import useAuth from "../../hooks/useAuth";
+
+// Importing Spinner component
 import Spinner from "../Spinner/Spinner";
 
+// Main PrivateRoute Component
 const PrivateRoute = ({ children, ...rest }) => {
+  // Getting functions from the hook
   const { user, isLoading } = useAuth();
+
+  // Showing a spinner while the page is reloading
   if (isLoading) {
     return <Spinner></Spinner>;
   }
   return (
+    // The main Route
     <Route
       {...rest}
       render={({ location }) =>
