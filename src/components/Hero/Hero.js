92be5a2ca93916e55337853dd4779b7aabde18b3
Moeen Mahmud <write2fahim007@gmail.com>
2021-10-30 15:40:07 +0600

    refactored hero component

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -17,10 +17,10 @@ const Hero = () => {
         <h3 className="text-gray-300 italic text-lg mb-3">
           Walk your own path and be yourself
         </h3>
-        <h1 className="text-white font-bold text-7xl mb-4">
+        <h1 className="text-white font-bold text-5xl lg:text-7xl mb-4">
           Travel <span className="text-indigo-600">Solo</span>
         </h1>
-        <p className="text-gray-200 font-semibold text-xl">
+        <p className="text-gray-200 font-semibold text-justify lg:text-center lg:text-xl px-4 lg:p-0">
           Going in a group is a great way for solo travelers to see the world.
           <br />
           Enjoy the ease of having all the details handled, and the peace
