d89d571325407e5925cc1d7adbeb43ed59eac139
Moeen Mahmud <write2fahim007@gmail.com>
2021-10-30 20:24:10 +0600

    refacotred firebase config codes

--- src/Firebase/firebase.init.js
+++ src/Firebase/firebase.init.js
@@ -1,6 +1,10 @@
+// Initialize firebase app
 import { initializeApp } from "firebase/app";
+
+// Importing firebse configuration
 import firebaseConfig from "./firebase.config";
 
+// Funtion for initializing auth
 const initializeAuthentication = () => {
   initializeApp(firebaseConfig);
 };
